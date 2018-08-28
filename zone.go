1145336b56b5d18f5ca4a0083c2afbd80205f181
caiges <caigesn@gmail.com>
2018-08-28 15:58:51 -0700

    Add JSON tags

--- zone.go
+++ zone.go
@@ -7,24 +7,27 @@ import (
 
 // Zone is a grouping of network resources.
 type Zone struct {
-	ID             int
-	IsDefault      bool
-	Name           string
-	Region         *map[string]bool
-	Status         string
-	ValidSourceHVS *map[string]types.NumericalBoolean
+	ID             int                                `json:"id,omitempty"`
+	IsDefault      types.NumericalBoolean             `json:"is_default,omitempty"`
+	Name           string                             `json:"name,omitempty"`
+	Region         *map[string]types.NumericalBoolean `json:"region,omitempty"`
+	Status         string                             `json:"status,omitempty"`
+	ValidSourceHVS *map[string]types.NumericalBoolean `json:"valid_source_hvs,omitempty"`
 }
 
+// ZoneParams are the set of parameters you can pass to the API for Network Zones.
 type ZoneParams struct {
 	ID int
 }
 
+// ZoneList is an envelope for the API result containing either a list of zones or an error.
 type ZoneList struct {
 	*client.LWAPIError
 	*client.ListMeta
 	Items []Zone
 }
 
+// ZoneItem is an envelope for the API result containing either a zones or an error.
 type ZoneItem struct {
 	*client.LWAPIError
 	Zone
