408eee42c9cdc8229d23f7b6b1473c7526a061db
Jack Hayhurst <jakdept@gmail.com>
2023-09-24 00:13:02 -0400

    we can't do test coverage, but I can show it's working?

--- examples/dns_client_test.go
+++ examples/dns_client_test.go
@@ -2,23 +2,27 @@ package network
 
 import (
 	"flag"
+	"log"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/liquidweb/liquidweb-go/client"
+	"github.com/liquidweb/liquidweb-go/network"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 var liveTest = flag.Bool("live", false, "set true to run live test")
-
-func init() {
-	flag.Parse()
-}
+var testZone = flag.String("zone", "", "given zone for dns tests")
 
 func TestLivePresent(t *testing.T) {
 	if !*liveTest {
 		t.Skip("skipping live test")
+	}
+	if strings.TrimSpace(*testZone) == "" {
+		t.Skip("skipping zone test without zone")
 	}
 
 	username, password := os.Getenv("LWAPI_USERNAME"), os.Getenv("LWAPI_PASSWORD")
@@ -28,11 +32,12 @@ func TestLivePresent(t *testing.T) {
 	api, err := client.NewAPI(username, password, url, timeout)
 	require.NoError(t, err, "failed to create API")
 
-	reqParams := DNSRecordParams{
-		Zone: os.Getenv("LWAPI_ZONE"),
+	reqParams := network.DNSRecordParams{
+		Zone: *testZone,
 	}
 
 	dnsRecords, err := api.NetworkDNS.List(&reqParams)
-	require.NoError(t, err)
+	require.NoError(t, err, "got an error using parameters %#v", reqParams)
 	assert.NotZero(t, len(dnsRecords.Items))
+	log.Printf("got back %s", spew.Sdump(dnsRecords))
 }
